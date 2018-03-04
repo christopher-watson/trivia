68670a61b0dd8fe0c9c80cca2a4deabfc723c144
Christopher Watson <christopher_watson@icloud.com>
2018-03-04 17:53:22 -0500

    music

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
 var $mute = $('#mute');
 var $startButton = $('#start-button');
 var $quit = $('#quit');
-// var audio = new Audio('./../music/Jeopardy-theme-song.mp3');
 
 var used = [];
 var quesNum = 0;
@@ -13,97 +12,97 @@ var correct = 0;
 var wrong = 0;
 var unAnswered = 0;
 
- var jeopardy = [
-    item = {
-      question: "What NFL team won Super Bowl 51",
-      rightAnswer: "New England Patriots",
-      answerArray: [
-        "New England Patriots",
-        "Dallas Cowboys",
-        "Los Angeles Chargers", 
-        "Atlanta Falcons"
-      ]
-    },
-    item = {
-      question: "Who was the NBA's Most Valuable Player (MVP) for 2017",
-      rightAnswer: "Russell Westbrook",
-      answerArray: [
-        "Draymond Green",
-        "Russell Westbrook",
-        "Kawhi Leonard",
-        "James Harden"
-      ]
-    },
-    item = {
-      question: "The 2017 Louis Vuitton Cup was awarded to Team New Zeland in which sport",
-      rightAnswer: "Sailing",
-      answerArray: [
-        "Tennis",
-        "Rugby",
-        "Sailing",
-        "Curling"
-      ]
-    },
-    item = {
-      question: "In what month did the 2017 Tour de France take place",
-      rightAnswer: "July",
-      answerArray: [
-        "June",
-        "July",
-        "May",
-        "April"
-      ]
-    },
-    item = {
-      question: "Which NHL team won the 2017 Stanley Cup finals against the Nashville Predators",
-      rightAnswer: "Pittsburgh Penguins",
-      answerArray: [
-        "Ottawa Senators",
-        "Pittsburgh Penguins",
-        "Columbus Blue Jackets",
-        "Washington Capitals"
-      ]
-    },
-    item = {
-      question: "What horse won the 2017 Kentucky Derby",
-      rightAnswer: "Always Dreaming",
-      answerArray: [
-        "Always Dreaming",
-        "Lookin At Lee",
-        "Classic Empire",
-        "Battle of Midway"
-      ]
-    },
-    item = {
-      question: "What country won the 2017 World Junior Ice Hockey Championship",
-      rightAnswer: "United States",
-      answerArray: [
-        "United States",
-        "Russia",
-        "Finland",
-        "Canada"
-      ]
-    },
-    item = {
-      question: "Who was the first pick of the 2017 NBA Draft",
-      rightAnswer: "Markelle Fultz",
-      answerArray: [
-        "Lonzo Ball",
-        "Jayson Tatum",
-        "Josh Jackson",
-        "Markelle Fultz"
-      ]
-    },
-    item = {
-      question: "Who was the first pick in the 2017 NFL Draft",
-      rightAnswer: "Myles Garrett",
-      answerArray: [
-        "Leonard Fournette",
-        "Mitchell Trubisky",
-        "Myles Garrett",
-        "Solomon Thomas"
-      ]
-    }
+var jeopardy = [
+  item = {
+    question: "What NFL team won Super Bowl 51",
+    rightAnswer: "New England Patriots",
+    answerArray: [
+      "New England Patriots",
+      "Dallas Cowboys",
+      "Los Angeles Chargers", 
+      "Atlanta Falcons"
+    ]
+  },
+  item = {
+    question: "Who was the NBA's Most Valuable Player (MVP) for 2017",
+    rightAnswer: "Russell Westbrook",
+    answerArray: [
+      "Draymond Green",
+      "Russell Westbrook",
+      "Kawhi Leonard",
+      "James Harden"
+    ]
+  },
+  item = {
+    question: "The 2017 Louis Vuitton Cup was awarded to Team New Zeland in which sport",
+    rightAnswer: "Sailing",
+    answerArray: [
+      "Tennis",
+      "Rugby",
+      "Sailing",
+      "Curling"
+    ]
+  },
+  item = {
+    question: "In what month did the 2017 Tour de France take place",
+    rightAnswer: "July",
+    answerArray: [
+      "June",
+      "July",
+      "May",
+      "April"
+    ]
+  },
+  item = {
+    question: "Which NHL team won the 2017 Stanley Cup finals against the Nashville Predators",
+    rightAnswer: "Pittsburgh Penguins",
+    answerArray: [
+      "Ottawa Senators",
+      "Pittsburgh Penguins",
+      "Columbus Blue Jackets",
+      "Washington Capitals"
+    ]
+  },
+  item = {
+    question: "What horse won the 2017 Kentucky Derby",
+    rightAnswer: "Always Dreaming",
+    answerArray: [
+      "Always Dreaming",
+      "Lookin At Lee",
+      "Classic Empire",
+      "Battle of Midway"
+    ]
+  },
+  item = {
+    question: "What country won the 2017 World Junior Ice Hockey Championship",
+    rightAnswer: "United States",
+    answerArray: [
+      "United States",
+      "Russia",
+      "Finland",
+      "Canada"
+    ]
+  },
+  item = {
+    question: "Who was the first pick of the 2017 NBA Draft",
+    rightAnswer: "Markelle Fultz",
+    answerArray: [
+      "Lonzo Ball",
+      "Jayson Tatum",
+      "Josh Jackson",
+      "Markelle Fultz"
+    ]
+  },
+  item = {
+    question: "Who was the first pick in the 2017 NFL Draft",
+    rightAnswer: "Myles Garrett",
+    answerArray: [
+      "Leonard Fournette",
+      "Mitchell Trubisky",
+      "Myles Garrett",
+      "Solomon Thomas"
+    ]
+  }
 ]
 
 function random(){
@@ -191,10 +190,6 @@ function decrement() {
         }
       }
     });
-    // setTimeout(function(){
-    //   gameReset();
-    //   runGame();
-    // }, 1000 * 2);
   }
 }
 function stop() {
@@ -222,8 +217,8 @@ function runGame(){
       theme: 'modern',
       buttons: {
         Yes: function () {
-            stop();
-            newGame();
+          stop();
+          newGame();
         },
         No: function () {
           $("#timer").text(":0");
@@ -233,7 +228,7 @@ function runGame(){
           $("#austin").show();          
         }
       }
-  });
+    });
   }
 }
 
@@ -258,19 +253,37 @@ function newGame(){
   $("#austin").hide(); 
 }
 
+var audio = document.getElementById("audio");
+var playing = false;
+audio.loop = true;
+function playAudio() { 
+  audio.play();
+  playing = true;
+} 
+function pauseAudio() { 
+  audio.pause();
+  playing = false;
+}
+
 $(document).on("click", ".answers", answerQuestion);
 $("#start-button").click(function(){
   runGame();
-  // audio.play();
   $("#start-button").hide();
   $("#question-container").show();
   $("#answer-container").show();
+  playAudio();
 });
-
-// $("#mute").on("click", audio.pause());
 
 $(".answers").click(function(){
   stop();
+})
+
+$("#play").click(function(){
+  playAudio();
+})
+
+$("#pause").click(function(){
+  pauseAudio();
 })
 
 $("#reset").click(function(){
@@ -279,13 +292,13 @@ $("#reset").click(function(){
     content: 'Are You Sure?',
     theme: 'modern',
     buttons: {
-        Yes: function () {
-            stop();
-            newGame();
-        },
-        No: function () {
-            // $.alert('Ok!');
-        }
+      Yes: function () {
+          stop();
+          newGame();
+      },
+      No: function () {
+          // return to game
+      }
     }
-});
+  });
 })
